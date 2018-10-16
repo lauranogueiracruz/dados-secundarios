8c66cd986c083173a498544f4d199f1bc680638e
lauranogueiracruz <laurancruz@gmail.com>
2018-10-16 20:32:42 -0300

    16_10_2018b

--- trabalhofinal_disciplinad6.R
+++ trabalhofinal_disciplinad6.R
@@ -9,4 +9,13 @@ gc()
 
 febre_amarela <- read_csv2("C:/Users/laura/Documents/dados-secundarios/febre_amarela.csv")
 
+FA <- read.csv2("C:/Users/laura/Documents/dados-secundarios/febre_amarela.csv")
+
+febre_amarela %>%
+  select(estado_requisitante, Agravo da Requisição, data_solicitacao, data_1_ sintomas )
+
+febre_amarela %>%
+  filter(year(dmy(data_decisao)) == 2018)
+  select(n_processo, data_decisao, data_registro) %>%
+  mutate(tempo = dmy(data_) - dmy(data_d))
 
